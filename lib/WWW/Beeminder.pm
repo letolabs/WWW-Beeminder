0a4346605e4868c9e70eda0a7df1787757794e51
Jonathan "Duke" Leto <jonathan@leto.net>
2012-06-29 13:01:20 -0700

    Add an add_data method

--- lib/WWW/Beeminder.pm
+++ lib/WWW/Beeminder.pm
@@ -1,9 +1,26 @@
 package WWW::Beeminder;
 use Moose;
 use JSON;
+use WWW::Mechanize;
 use namespace::autoclean;
 
+has base_url => ( is => 'rw', isa => 'Str', default => 'https://www.beeminder.com' );
+
+has username => ( is => 'rw', isa => 'Str', default => '' );
+
 our $VERSION = '0.01';
+
+sub add_data {
+    my ($self, $data) = @_;
+
+    unless ($self->username) {
+        die "You must specify a username before adding data";
+    }
+
+    my $mech = WWW::Mechanize->new;
+    my $r    = $mech->post( $self->base_url(), $data );
+}
+
 
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
